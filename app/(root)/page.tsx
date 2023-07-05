869599caa511a5328b1d5468a86cd1be0d2bbdf4
Ckm54 <mutaic84@gmail.com>
2023-07-05 16:20:47 +0300

    components and layout setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div className="p-4">
+      <Button size={"default"} variant={"outline"}>
+        Click here
+      </Button>
+    </div>
+  );
+};
+
+export default Home;
