6eadb9f9670e0a10c037e7f2fd2554c6c453133c
Ckm54 <mutaic84@gmail.com>
2023-07-05 16:39:58 +0300

    style: update layouts and page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 
-const Home = () => {
+const SetupPage = () => {
   return (
     <div className="p-4">
       <Button size={"default"} variant={"outline"}>
@@ -10,4 +10,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default SetupPage;
