620faff85abbd3580716b70a9e5d1403f26a3ac3
Ckm54 <mutaic84@gmail.com>
2023-07-06 10:14:59 +0300

    :hammer: user create store and redirects

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,28 @@
+import prismaDb from "@/lib/prismaDb";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+export default async function SetupLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  // load the first store user has
+  const store = await prismaDb.store.findFirst({
+    where: {
+      userId,
+    },
+  });
+
+  if (store) {
+    redirect(`/${store.id}`);
+  }
+
+  return <>{children}</>;
+}
