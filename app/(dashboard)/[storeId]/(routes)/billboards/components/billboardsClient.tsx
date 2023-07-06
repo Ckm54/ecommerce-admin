aff2c5443a5ffa9438cae3f459162d33890c71c9
Ckm54 <mutaic84@gmail.com>
2023-07-06 18:02:12 +0300

    clientside billboard

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/billboardsClient.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import React from "react";
+
+import { Heading } from "@/components/ui/Heading";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { BillboardColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+
+interface BillboardClientProps {
+  billboardsData: BillboardColumn[];
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({
+  billboardsData,
+}) => {
+  const router = useRouter();
+  const params = useParams();
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Billboards (${billboardsData.length})`}
+          description="Manage billboards for your store"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+
+      {/* DATA TABLE */}
+      <DataTable columns={columns} data={billboardsData} searchKey="label" />
+    </>
+  );
+};
