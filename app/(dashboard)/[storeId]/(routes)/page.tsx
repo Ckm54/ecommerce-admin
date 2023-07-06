86af1e83d14eceaefbc3488787d67353932ae9c3
Ckm54 <mutaic84@gmail.com>
2023-07-06 11:38:43 +0300

    update dashboard page

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,5 +1,21 @@
-const DashboardPage = () => {
-  return <div>This is the DashboardPage</div>;
+import prismaDb from "@/lib/prismaDb";
+
+interface DashboardPageProps {
+  params: { storeId: string };
+}
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+  const store = await prismaDb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  });
+  return (
+    <div>
+      This is the DashboardPage
+      <p>Active store {store?.name}</p>
+    </div>
+  );
 };
 
 export default DashboardPage;
