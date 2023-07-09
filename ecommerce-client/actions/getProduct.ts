3e2a1ec2be84c718206e393ea13a2d6c7bfc39e4
Ckm54 <mutaic84@gmail.com>
2023-07-09 13:50:52 +0300

    :hammer: update utility functions

---
+++ ecommerce-client/actions/getProduct.ts
@@ -0,0 +1,11 @@
+import { ProductType } from "@/types";
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
+
+const getProduct = async (id: string): Promise<ProductType> => {
+  const response = await fetch(`${URL}/${id}`);
+
+  return response.json();
+};
+
+export default getProduct;
--- ecommerce-client/actions/getRelatedProducts.ts
+++ ecommerce-client/actions/getRelatedProducts.ts
@@ -4,25 +4,25 @@ import { ProductType } from "@/types";
 
 interface Query {
   categoryId?: string;
-  colorId?: string;
-  sizeId?: string;
-  isFeatured?: boolean;
 }
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
 
-const getProducts = async (query: Query): Promise<ProductType[]> => {
+const getRelatedProducts = async ({
+  id,
+  query,
+}: {
+  id: string;
+  query: Query;
+}): Promise<ProductType[]> => {
   const params = {
-    colorId: query.colorId,
-    sizeId: query.sizeId,
     categoryId: query.categoryId,
-    isFeatured: query.isFeatured,
   };
-  const url = `${URL}?${queryString.stringify(params)}`;
+  const url = `${URL}/${id}/related?${queryString.stringify(params)}`;
 
   const response = await fetch(url);
 
   return response.json();
 };
 
-export default getProducts;
+export default getRelatedProducts;
