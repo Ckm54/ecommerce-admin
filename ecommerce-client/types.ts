5b425b45473de7412e45c14d61281d4e46b0b706
Ckm54 <mutaic84@gmail.com>
2023-07-09 11:05:29 +0300

    better typing

--- ecommerce-client/types.ts
+++ ecommerce-client/types.ts
@@ -9,3 +9,26 @@ export interface Category {
   name: string;
   billboard: BillboardType;
 }
+
+export interface ProductType {
+  id: string;
+  category: Category;
+  name: string;
+  price: number;
+  size: Size;
+  color: Color;
+  images: Image[];
+}
+
+export interface Color extends Size {}
+
+export interface Image {
+  id: string;
+  url: string;
+}
+
+export interface Size {
+  id: string;
+  name: string;
+  value: string;
+}
